a264e7b7135e9332d4ed5048d64f7e9553d166e8
tianKong <1239423535@qq.com>
2018-02-22 14:14:55 +0800

    left_menu

--- js/index.js
+++ js/index.js
@@ -1,15 +1,27 @@
-/*search ctrl start*/
 	$(function(){
+		/*search ctrl start*/
 		/*搜索框...*/
 		$("#search").mouseenter(function(){
 			$("#search").addClass("enter").removeClass("search_input");
 		}).mouseout(function(){
 			$("#search").removeClass("enter").addClass("search_input");
 		})
-	})
-	/*导航...*/
-	$(".sky_header_nav li:not(:first-child,:last-child)").on("click",function(){
-		$(this).addClass("active").siblings(true).removeClass("active");
+		/*导航...*/
+		$(".sky_header_nav li:not(:first-child,:last-child)").on("click",function(index){
+			$(this).addClass("active").siblings(true).removeClass("active");
+			if ($(this)[0].innerHTML === "技术分享") {
+				$(".left_menu").css("display","block");
+			} else{
+				$(".left_menu").css("display","none");
+			}
+		})
+		/*search ctrl end*/
+		
+		/*left_menu start*/
+		    $('.left_menu_change dl dt').click(function(){
+		        $(this).siblings('dd').slideToggle();
+		    });
+		/*left_menu end*/
 	})
 
-/*search ctrl end*/
+
